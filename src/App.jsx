d24df1d486f4dd307492efd5069d249522926bdb
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-15 17:34:07 +0200

    mobile screen height fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Card from "./Card";
+
 function App() {
   return (
-    <main className=" h-screen py-20 px-8 font-manrope md:flex md:justify-center md:items-center bg-Light-Grayish-Blue">
+    <main className=" min-h-screen py-20 px-8 font-manrope md:flex md:flex-col md:justify-center md:items-center bg-Light-Grayish-Blue ">
       <Card />
     </main>
   );
