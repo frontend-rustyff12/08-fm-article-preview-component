d64a4e9a80036805b984d18f4e000d46d43af0f6
Russell <75209707+rustyff12@users.noreply.github.com>
2025-08-15 10:16:34 +0200

    mobile state and ui change

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 export default function Card() {
+  const [clicked, setClicked] = useState(!false);
+
   return (
-    <article className="bg-white rounded-xl md:w-4xl md:grid md:grid-cols-[40%_60%] md:grid-rows-[1fr, auto] ">
+    <article
+      className="
+        rounded-xl md:w-4xl md:grid md:grid-cols-[40%_60%] md:grid-rows-[1fr, auto]"
+    >
       <img
         className="rounded-t-xl h-56 w-full md:h-full  md:row-span-2 md:rounded-t-none md:rounded-l-xl"
         src="/drawers.jpg"
@@ -18,27 +24,53 @@ export default function Card() {
           you make any room feel complete.
         </p>
       </div>
-      <div className="grid grid-cols-[50px_minmax(150px,_1fr)_50px] items-center  px-8 py-4 md:col-start-2">
-        <img
-          className="h-10 w-auto rounded-full"
-          src="/avatar-michelle.jpg"
-          alt="Picture of Michelle"
-        />
-        <div>
-          <h3 className="font-bold text-Very-Dark-Grayish-Blue text-base">
-            Michelle Appleton
-          </h3>
-          <p className="text-Grayish-Blue text-sm">28 Jun 2020</p>
-        </div>
 
-        <input
-          className="w-10 p-2.5 h-auto rounded-full bg-Light-Grayish-Blue"
-          type="image"
-          id="share"
-          alt="share"
-          src="/icon-share.svg"
-        />
-      </div>
+      {clicked === true ? (
+        <div className="grid grid-cols-[50px_minmax(150px,_1fr)_50px] items-center  px-8 py-4 md:col-start-2">
+          <img
+            className="h-10 w-auto rounded-full"
+            src="/avatar-michelle.jpg"
+            alt="Picture of Michelle"
+          />
+          <div>
+            <h3 className="font-bold text-Very-Dark-Grayish-Blue text-base">
+              Michelle Appleton
+            </h3>
+            <p className="text-Grayish-Blue text-sm">28 Jun 2020</p>
+          </div>
+
+          <button
+            className="w-9 p-2.5 h-auto rounded-full bg-Light-Grayish-Blue cursor-pointer"
+            onClick={() => setClicked((prev) => !prev)}
+          >
+            <img src="/icon-share.svg" alt="share" />
+          </button>
+        </div>
+      ) : (
+        <div className="grid grid-cols-3 gap-4  items-center justify-items-center px-8 py-4 md:col-start-2 bg-Very-Dark-Grayish-Blue rounded-b-xl">
+          <h3 className="uppercase tracking-[0.2em] text-Grayish-Blue">
+            Share
+          </h3>
+          <div className="flex gap-3">
+            <a href="#">
+              <img src="/icon-facebook.svg" alt="Facebook link" />
+            </a>
+            <a href="#">
+              <img src="/icon-twitter.svg" alt="Twitter link" />
+            </a>
+            <a href="#">
+              <img src="/icon-pinterest.svg" alt="Pinterestlink" />
+            </a>
+          </div>
+
+          <button
+            className="w-9 p-2.5 h-auto rounded-full bg-Light-Grayish-Blue cursor-pointer"
+            onClick={() => setClicked((prev) => !prev)}
+          >
+            <img src="/icon-share.svg" alt="share" />
+          </button>
+        </div>
+      )}
     </article>
   );
 }
